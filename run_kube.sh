f6bcc37fe22e30cd563582d638dd66f1f7dc4336
Agoston Pajzs <agostonp@gmail.com>
2020-05-07 14:58:28 +0000

    Add more kubernetes commands: expose, scale, update(set image), rollback(rollout undo)

--- run_kube.sh
+++ run_kube.sh
@@ -12,3 +12,21 @@ echo Name of the Pod: $POD_NAME
 
 # Listen on port 80 locally, forwarding to 5000 in the pod
 kubectl port-forward pod/helloflaskdemo-69c5bf47cb-ngk7n 80:5000
+
+# A better way of exposing the container to outside
+kubectl expose deployment/helloflaskdemo --type="LoadBalancer" --port=80 --target-port=5000
+
+# Test that the application is up
+curl localhost?name=Mate
+
+# Scale the application to have 2 instances (replicas)
+kubectl scale deployments/helloflaskdemo --replicas=2
+
+# Show pods with IP addresses
+kubectl get pods -o wide
+
+# Perform a rolling update
+kubectl set image deployments/helloflaskdemo helloflaskdemo=agostonp/helloflask:v2
+
+# To roll back an update
+kubectl rollout undo deployments/helloflaskdemo
